a1dbb1dabec1de5cb9e4c2ef75ef6c957eb82c06
Dean Hyde <dean.hyde@usaycompare.com>
2018-05-11 14:36:24 +0100

    Added lumen check

--- helpers/functions.php
+++ helpers/functions.php
@@ -10,3 +10,14 @@ if ( ! function_exists('config_path')) {
         return app()->basePath() . '/config' . ($path ? '/' . $path : $path);
     }
 }
+
+if ( ! function_exists('is_lumen')) {
+    /**
+     * Check if we're in a lumen application
+     * @return bool
+     */
+    function is_lumen()
+    {
+        return str_contains(str_app()->version(), 'Lumen');
+    }
+}
--- src/Queue/QueueServiceProvider.php
+++ src/Queue/QueueServiceProvider.php
@@ -11,17 +11,13 @@ class QueueServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->app->booted(function() {
-            /** @var QueueManager $queue */
-            $queue = $this->app['queue'];
-            $connector = new V9QueueConnector;
-            $queue->stopping(function () use ($connector) {
-                $connector->connection()->close();
+        if (is_lumen()) {
+            $this->load();
+        } else {
+            $this->app->booted(function () {
+                $this->load();
             });
-            $queue->addConnector('rabbitmq', function () use ($connector) {
-                return $connector;
-            });
-        });
+        }
     }
 
     /**
@@ -31,5 +27,18 @@ class QueueServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->mergeConfigFrom(__DIR__ . "./../../config/queue.php", "queue.connections.rabbitmq");
+    }
+
+    public function load()
+    {
+        /** @var QueueManager $queue */
+        $queue = $this->app['queue'];
+        $connector = new V9QueueConnector;
+        $queue->stopping(function () use ($connector) {
+            $connector->connection()->close();
+        });
+        $queue->addConnector('rabbitmq', function () use ($connector) {
+            return $connector;
+        });
     }
 }
